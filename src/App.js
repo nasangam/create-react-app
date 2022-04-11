a2088161225245110df16ae40ba8c934e171ad97
nasangam <nagaraju_sangam@yahoo.co.in>
2022-04-11 22:47:27 +0530

    upd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     Hello world Updated 444
-    </div>
-  );
+  return <div className="App">Hello world Updated 555</div>;
 }
 
 export default App;
